d92636c04e108f241adc1bfdc745489b53bf7176
Arjun Dhakal <arjun.nbsm@yahoo.com>
2023-03-14 23:36:20 -0400

    php - day 1 class acitivity and assignment

---
+++ day01hello/2hellofrom.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+    function printForm($nameVal="", $ageVal=""){
+        $form = <<< END
+        <form>
+        Name: <input type="text" name="name" value="$nameVal"><br>
+        Age: <input type="number" name="age" value="$ageVal"><br>
+        <input type="submit" value="Say hello">
+        </form>
+        END;
+        echo $form;
+    }
+    if (isset($_GET["name"])){
+      $name = $_GET["name"];
+      $age = $_GET["age"];
+      $errorList = [];
+      // name must be 2-20 charecters long
+      if (strlen($name)<2 || strlen($name)>20){
+        $errorList[]="Name must be 2-20 characters log";
+        $name="";
+      }
+      // age must be a integer, 1-150 value
+      if(!is_numeric($age) || $age<1 || $age >150){
+        $errorList[]="Age must be 1-150";
+        $age="";
+      }
+
+        if ($errorList){
+          echo "<p>Submission failed, errors found:</p>\n";
+          echo"<ul>\n";
+          foreach($errorList as $error){
+            echo "<li>$error</li>\n";           
+          
+          }
+          echo "</ul>\n";
+          printForm($name, $age);        
+        }
+        else{
+          echo "<p>Hi $name, yur are $age y/o. Nice to meet you.</p>";
+        }
+    } else {
+      printForm();
+    }
+  ?> 
+</body>
+</html>
